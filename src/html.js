58b7521efc26c8267daa5864db923d9b69fb150c
Administrator <doan.trong.quoc.binh@gmail.com>
2018-05-19 08:44:14 +0700

    up

---
+++ src/html.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const BUILD_TIME = new Date().getTime();
+
+export default class HTML extends React.Component {
+  static propTypes = {
+    body: PropTypes.string,
+  };
+
+  render() {
+    let css;
+    if (process.env.NODE_ENV === 'production') {
+      css = (
+        <style
+          dangerouslySetInnerHTML={{
+            __html: require('!raw!../public/styles.css'),
+          }}
+        />
+      );
+    }
+
+    return (
+      <html lang="en">
+        <head>
+		<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script> (adsbygoogle = window.adsbygoogle || []).push({
+google_ad_client: "ca-pub-1521295516322500",
+enable_page_level_ads: true
+});
+</script>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          {this.props.headComponents}
+          {css}
+        </head>
+        <body itemScope itemType="http://schema.org/WebPage">
+          <div
+            id="___gatsby"
+            dangerouslySetInnerHTML={{ __html: this.props.body }}
+          />
+          {this.props.postBodyComponents}
+        </body>
+      </html>
+    );
+  }
+}
