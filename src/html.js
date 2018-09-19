5c39738c9d59abdd468c1bc5e92165f0710f06ee
Administrator <doan.trong.quoc.binh@gmail.com>
2018-09-19 14:00:49 +0700

    up

--- src/html.js
+++ src/html.js
@@ -25,7 +25,7 @@ export default class HTML extends React.Component {
         <head>
 		<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <script> (adsbygoogle = window.adsbygoogle || []).push({
-google_ad_client: "ca-pub-1469239684023873",
+google_ad_client: "ca-pub-here",
 enable_page_level_ads: true
 });
 </script>
